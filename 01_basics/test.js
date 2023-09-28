64f4e7ac8df7c8593b047a6832db2ce50b54887f
Bhavik Sharma <120740091+sharmabhavik@users.noreply.github.com>
2023-09-28 13:38:01 +0000

    configuration

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,29 @@
 console.log("Bhavik")
+
+let yuji = "bhaisaheb"
+console.log(typeof(yuji))
+
+//converting from number to string
+let gojo = Number(yuji)
+console.log(typeof(gojo))
+console.log(gojo)
+
+let isloggedIn = "bhavik"
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn)
+
+let booleanno = true
+let no = Number(booleanno)
+console.log(no)
+
+const dust = "Bro"
+let dusty = "Rammi"
+
+// Alert is not defined here it can be used in browser only.
+//alert("Bhavik is there")
+
+let accountId = 221993
+let accountcity = "Ahmedabad"
+
+console.table([accountId. accountcity, yuji, gojo, no, booleanno, dust, dusty])
+
