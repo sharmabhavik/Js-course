b56706ad3fcc89fafa0ed657d7e8b78c37089a39
Bhavik Sharma <bhaviksharma1312@gmail.com>
2023-11-08 16:18:33 +0530

    good

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,29 +1,6 @@
-console.log("Bhavik")
+let somenumber = 33
 
-let yuji = "bhaisaheb"
-console.log(typeof(yuji))
-
-//converting from number to string
-let gojo = Number(yuji)
-console.log(typeof(gojo))
-console.log(gojo)
-
-let isloggedIn = "bhavik"
-let booleanisloggedIn = Boolean(isloggedIn)
-console.log(booleanisloggedIn)
-
-let booleanno = true
-let no = Number(booleanno)
-console.log(no)
-
-const dust = "Bro"
-let dusty = "Rammi"
-
-// Alert is not defined here it can be used in browser only.
-//alert("Bhavik is there")
-
-let accountId = 221993
-let accountcity = "Ahmedabad"
-
-console.table([accountId. accountcity, yuji, gojo, no, booleanno, dust, dusty])
+let stringno = String(somenumber)
+console.log(stringno)
+console.log(typeof stringno)
 
---
+++ 01_basics/test2.js
@@ -0,0 +1,22 @@
+//Operations
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let firstvalue = 28;
+let secondvalue = 38;
+let thirdvalue = firstvalue + secondvalue;
+console.log(thirdvalue);
+
+console.log(3 * 3); // 3 multiplies by 3
+console.log(3 ** 4); // 3 ki power 4
+console.log(3 / 4); // 3 ki power 4
+console.log(3 % 4); // 3 ki power 4
+console.log(3 + 4); // 3 ki power 4
+console.log(3 - 4); // 3 ki power 4
+
+let str1 = "Glenn";
+let str2 = "Maxx";
+
+let str3 = str1 + str2;
+console.log(str3);
